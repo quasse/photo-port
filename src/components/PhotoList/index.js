212a639607e422c5752c30b99814327f6f40f695
Sam Morgen <samorgen14@gmail.com>
2021-09-10 20:02:57 -0700

    fixed photo

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -123,14 +123,17 @@ const PhotoList = ({ category }) => {
   return (
     <div>
       <div className="flex-row">
-        {currentPhotos.map((image, i) => (
-          <img
-            src={`../../assets/small/${category}/${i}.jpg`}
-            alt={image.name}
-            className="img-thumbnail mx-1"
-            key={image.name}
-          />
-        ))}
+        {currentPhotos.map((image, i) => {
+          console.log(require(`../../assets/small/${category}/${i}.jpg`));
+          return (
+            <img
+              src={require(`../../assets/small/${category}/${i}.jpg`).default}
+              alt={image.name}
+              className="img-thumbnail mx-1"
+              key={image.name}
+            />
+          );
+        })}
       </div>
     </div>
   );
